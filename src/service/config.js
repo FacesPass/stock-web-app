aff3d843f7e25af3717896bbcc86ba29633c6f2d
JiquanWang99 <951873902@qq.com>
2021-06-20 17:48:41 +0800

    feat: 修改配置

--- src/service/config.js
+++ src/service/config.js
@@ -1,3 +1,3 @@
-export const env = process.env.NODE_ENV === 'development' ?
-  'http://k92es6.natappfree.cc' :  //测试接口URL
-  ''    //线上接口URL
+export const env = process.env.NODE_ENV === 'development'
+  ? 'http://82.157.30.227:9090' //测试接口URL  
+  : 'http://82.157.30.227:9090' //线上接口URL
